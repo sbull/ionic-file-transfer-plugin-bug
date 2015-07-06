89bcf12808efd202455538062e9861b0ba89c635
Steven Bull <steven@thebulls.us>
2015-07-06 16:21:25 -0700

    PREVIOUS BROKEN: Rearranged plugins.

--- www/js/app.js
+++ www/js/app.js
@@ -20,7 +20,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
 
     try {
-      alert('hello ionic-tabs: Removed JS.');
+      alert('hello ionic-tabs: Rearranged plugins.');
       alert('File '+(typeof File)+': '+JSON.stringify(File));
       alert('FileTransfer '+(typeof FileTransfer)+': '+JSON.stringify(FileTransfer));
       alert('FileTransferError '+(typeof FileTransferError)+': '+JSON.stringify(FileTransferError));
