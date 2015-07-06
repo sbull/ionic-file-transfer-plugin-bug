9f927d152f0599fad515a601c5702b5969365cc0
Steven Bull <steven@thebulls.us>
2015-07-06 16:46:43 -0700

    Previous working. Re-added params.

--- www/js/app.js
+++ www/js/app.js
@@ -20,7 +20,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
 
     try {
-      alert('hello ionic-tabs: Updated cordova, removed params.');
+      alert('hello ionic-tabs: Rolled back params.');
       alert('File '+(typeof File)+': '+JSON.stringify(File));
       alert('FileTransfer '+(typeof FileTransfer)+': '+JSON.stringify(FileTransfer));
       alert('FileTransferError '+(typeof FileTransferError)+': '+JSON.stringify(FileTransferError));
