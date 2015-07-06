b4e98cdfaaeab1de33ee5f5acfb813f8b414e998
Steven Bull <steven@thebulls.us>
2015-07-06 16:09:08 -0700

    Changed plugins to old versions.

--- www/js/app.js
+++ www/js/app.js
@@ -20,7 +20,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
 
     try {
-      alert('hello ionic-tabs: 3rd-party JS loaded.');
+      alert('hello ionic-tabs: old plugins loaded.');
       alert('File '+(typeof File)+': '+JSON.stringify(File));
       alert('FileTransfer '+(typeof FileTransfer)+': '+JSON.stringify(FileTransfer));
       alert('FileTransferError '+(typeof FileTransferError)+': '+JSON.stringify(FileTransferError));
